101dd69c9e99bfde2bbc92a1c75504453168bcb5
Sandy <dinhduygiangvo@gmail.com>
2024-10-26 19:55:38 +0700

    The newest update.
    This update included a quiz and a submit function. However, the submit function is not working correctly. Will fix later.

---
+++ main.js
@@ -0,0 +1,60 @@
+/// Questions handling and stuff
+
+// Logging all questions
+let all_questions = document.getElementsByClassName("question");
+let all_answers = document.getElementsByClassName("multiple-choice");
+
+let answers = [0, 4, 9, 12, 15]; // where we store the correct answers
+let correct = 0;
+let quiz_isfinished = false;
+function findself(identity) {
+    for (let i=0; i<all_answers.length; i++) {
+        if (identity == all_answers[i]) {
+            let whole_list = identity.parentNode;
+            for (let j=0; j<whole_list.childNodes.length; j++) {
+                if (whole_list.childNodes[j].nodeName == "LI") { 
+                    whole_list.childNodes[j].style.backgroundColor = "transparent";
+                }
+            }
+            all_answers[i].style.backgroundColor = "grey";
+        }
+    }
+}
+function return_answer(event) {
+    findself(event.target);
+}
+
+function addClicker() {
+    for (let i=0; i<all_answers.length; i++) {
+        document.getElementsByClassName("multiple-choice")[i].onclick = return_answer;
+    }
+}
+
+function submit_answer() {
+    for (let i=0; i<answers.length; i++) {
+        all_answers[answers[i]].style.backgroundColor = "yellow";
+    }
+    for (let i=0; i<all_answers.length; i++) {
+        if (all_answers[i].style.backgroundColor == "grey") {
+            if (answers.includes(i)) {
+                correct ++;
+                all_answers[i].style.backgroundColor = "black";
+            } else {
+                all_answers[i].style.backgroundColor = "red";
+            }
+        }
+    }
+    console.log(correct);
+}
+addClicker();
+document.getElementById("submit-button").onclick = submit_answer;
+
+// DO A SYSTEM WHERE THE STUDENT MUST SELECT THE ANSWERS FIRST WITHOUT KNOWING WHETHER THEY GOT IT RIGHT.
+// THEN, THERE WILL BE A SUBMIT BUTTON TO SEE HOW WELL THE STUDENT DID, WHICH WOULD BE DISPLAYED ON THE "QUIZ FOR TODAY!" SECTION.
+// IF THE STUDENT GETS EVERYTHING RIGHT, THEY WILL RECEIVE A LINK TO A WEBSITE WHERE THEY COULD TYPE THEIR NAMES OR JUST SCREENSHOT TO GET HOUSEPOINTS.
+// HOWEVER, IF THE STUDENT COPIES THE LINK, THERE NEEDS TO BE A CONDITION CHECKING IF THEY ACTUALLY GOT IT RIGHT.
+
+/*
+if (answers.includes(i)) {
+                identity.style.backgroundColor = "grey";
+            }*/
