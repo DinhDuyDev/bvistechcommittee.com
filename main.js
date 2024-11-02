ee15647ebf4dc824ff42ffaf88a4405a229534e5
Sandy <dinhduygiangvo@gmail.com>
2024-11-02 22:00:41 +0700

    EVERY NEW UPDATE!!
    THE SUBMIT FUNCTION WORKS

--- main.js
+++ main.js
@@ -7,16 +7,19 @@ let all_answers = document.getElementsByClassName("multiple-choice");
 let answers = [0, 4, 9, 12, 15]; // where we store the correct answers
 let correct = 0;
 let quiz_isfinished = false;
+
 function findself(identity) {
-    for (let i=0; i<all_answers.length; i++) {
-        if (identity == all_answers[i]) {
-            let whole_list = identity.parentNode;
-            for (let j=0; j<whole_list.childNodes.length; j++) {
-                if (whole_list.childNodes[j].nodeName == "LI") { 
-                    whole_list.childNodes[j].style.backgroundColor = "transparent";
+    if (!quiz_isfinished) {
+        for (let i=0; i<all_answers.length; i++) {
+            if (identity == all_answers[i]) {
+                let whole_list = identity.parentNode;
+                for (let j=0; j<whole_list.childNodes.length; j++) {
+                    if (whole_list.childNodes[j].nodeName == "LI") { 
+                        whole_list.childNodes[j].style.backgroundColor = "transparent";
+                    }
                 }
+                all_answers[i].style.backgroundColor = "grey";
             }
-            all_answers[i].style.backgroundColor = "grey";
         }
     }
 }
@@ -31,20 +34,30 @@ function addClicker() {
 }
 
 function submit_answer() {
-    for (let i=0; i<answers.length; i++) {
-        all_answers[answers[i]].style.backgroundColor = "yellow";
-    }
-    for (let i=0; i<all_answers.length; i++) {
-        if (all_answers[i].style.backgroundColor == "grey") {
-            if (answers.includes(i)) {
-                correct ++;
-                all_answers[i].style.backgroundColor = "black";
-            } else {
-                all_answers[i].style.backgroundColor = "red";
+    if (!quiz_isfinished) {
+        for (let i=0; i<all_answers.length; i++) {
+            if (all_answers[i].style.backgroundColor == "grey") {
+                if (answers.includes(i)) {
+                    correct ++;
+                    all_answers[i].style.backgroundColor = "green";
+                } else {
+                    all_answers[i].style.backgroundColor = "red";
+                }
             }
         }
+        for (let i=0; i<all_answers.length; i++) {
+            if (all_answers[i].style.backgroundColor != "green") {
+                if (answers.includes(i)) {
+                    all_answers[i].style.backgroundColor = "yellow";
+                }
+            }
+        }
+        quiz_isfinished = true;
+        console.log(correct);
+        if (correct == 5) {
+            document.getElementById("congrats").style.display = "block";
+        }
     }
-    console.log(correct);
 }
 addClicker();
 document.getElementById("submit-button").onclick = submit_answer;
